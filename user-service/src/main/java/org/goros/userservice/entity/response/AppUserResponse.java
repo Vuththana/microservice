bd13eb71f9ce89257e95d50765147f6e34db8c95
Vuththana <135146361+Vuththana@users.noreply.github.com>
2026-07-05 20:44:14 +0700

    added service

--- user-service/src/main/java/org/goros/userservice/entity/response/AppUserResponse.java
+++ user-service/src/main/java/org/goros/userservice/entity/response/AppUserResponse.java
@@ -1,4 +1,14 @@
 package org.goros.userservice.entity.response;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class AppUserResponse {
+    public UUID userId;
+    public String username;
+    public String firstName;
+    public String lastName;
+    public String email;
 }
